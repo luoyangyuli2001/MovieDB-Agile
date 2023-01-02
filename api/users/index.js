8a0d7f0cc35896a087a1e6e32003dc503482dc1b
Luo Yang <luoyangyul2001@gmail.com>
2023-01-02 02:44:39 +0000

    User - Tested APIs related to users and modified unreasonable code.

--- api/users/index.js
+++ api/users/index.js
@@ -38,7 +38,7 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
     const movie = await movieModel.findByMovieDBId(newFavourite);
     const user = await User.findByUserName(userName);
     if (user.favourites.includes(movie.id)) {
-        res.status(201).json({code: 201, msg: 'Already exists in favourites.'})
+        res.status(403).json({code: 403, msg: 'Already exists in favourites.'})
     } else {
         await user.favourites.push(movie.id);
         await user.save(); 
@@ -48,13 +48,18 @@ router.post('/:userName/favourites', asyncHandler(async (req, res) => {
 
 //Delete a favourite
 router.post('/:username/movie/:id/favourites', asyncHandler(async (req, res) => {
-  const newFavourite = req.params.id;
+  const favouriteToDelete = req.params.id;
   const userName = req.params.username;
   const user = await User.findByUserName(userName);
-  const index = user.favourites.indexOf(newFavourite)
-  await user.favourites.splice(index, 1);
-  await user.save(); 
-  return res.status(201).json(user); 
+  if(user.favourites.includes(favouriteToDelete)) {
+    const index = user.favourites.indexOf(favouriteToDelete)
+    await user.favourites.splice(index, 1);
+    await user.save(); 
+    return res.status(201).json(user); 
+  }
+  else{
+    res.status(404).json({ code: 404, msg: "Not in favourites."})
+  }
 }));
 
 // Register OR authenticate a user
--- tests/functional/api/users/index.js
+++ tests/functional/api/users/index.js
@@ -2,6 +2,7 @@ import chai from "chai";
 import request from "supertest";
 const mongoose = require("mongoose");
 import User from "../../../../api/users/userModel";
+import users from "../../../../seedData/users";
 import api from "../../../../index";
 
 const expect = chai.expect;
@@ -35,6 +36,9 @@ describe("Users endpoint", () => {
       await request(api).post("/api/users?action=register").send({
         username: "user2",
         password: "test2",
+      });
+      await request(api).post("/api/users/user1/favourites").send({
+        id: 114514
       });
     } catch (err) {
       console.error(`failed to Load user test Data: ${err}`);
@@ -73,36 +77,121 @@ describe("Users endpoint", () => {
             .expect(201)
             .expect({ msg: "Successful created new user.", code: 201 });
         });
-        after(() => {
-          return request(api)
+        after(async () => {
+          const res = await request(api)
             .get("/api/users")
             .set("Accept", "application/json")
             .expect("Content-Type", /json/)
-            .expect(200)
-            .then((res) => {
-              expect(res.body.length).to.equal(3);
-              const result = res.body.map((user) => user.username);
-              expect(result).to.have.members(["user1", "user2", "user3"]);
-            });
+            .expect(200);
+          expect(res.body.length).to.equal(3);
+          const result = res.body.map((user) => user.username);
+          expect(result).to.have.members(["user1", "user2", "user3"]);
         });
       });
     });
     describe("For an authenticate action", () => {
       describe("when the payload is correct", () => {
-        it("should return a 200 status and a generated token", () => {
-          return request(api)
+        it("should return a 200 status and a generated token", async () => {
+          const res = await request(api)
             .post("/api/users?action=authenticate")
             .send({
               username: "user1",
               password: "test1",
             })
-            .expect(200)
+            .expect(200);
+          expect(res.body.success).to.be.true;
+          expect(res.body.token).to.not.be.undefined;
+          user1token = res.body.token.substring(7);
+        });
+      });
+    });
+  });
+
+  describe("POST /api/users/:username/favourites", () => {
+    describe("for valid user name", () => {
+      describe("when the movie is not in favourites", () => {
+        it("should return user message and a status 201", async () => {
+          const res = await request(api)
+            .post(`/api/users/${users[0].username}/favourites`)
+            .send({
+              id: 462750
+            })
+            .set("Accept", "application/json")
+            .expect(201);
+          expect(res.body).to.have.property("username", users[0].username);
+          expect(res.body.favourites.length).to.equal(2);
+        });
+      });
+      describe("when the movie is in favourites", () => {
+        it("return error message and a status 403", () => {
+          return request(api)
+            .post(`/api/users/${users[0].username}/favourites`)
+            .send({
+              id: 114514
+            })
+            .set("Accept", "application/json")
+            .expect(403)
+            .expect({ code: 403, msg: 'Already exists in favourites.' })
+        });
+      });
+    });
+    describe("for invalid user name", () => {
+      it("return error message and a status 500", () => {
+        return request(api)
+          .get(`/api/users/;'--=&*/favourites`)
+          .set("Accept", "application/json")
+          .expect("Content-Type", "text/html; charset=utf-8")
+          .expect(500)
+      });
+    });
+  });
+
+  describe("GET /api/users/:username/favourites", () => {
+    it("should return the favourites list and status 200", async () => {
+      const res = await request(api)
+        .get(`/api/users/${users[0].username}/favourites`)
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200);
+      expect(res.body).to.be.a("array");
+      // 114514 is the movie id added in before()
+      expect(res.body).to.contains(114514);
+    });
+  });
+
+  describe("POST /api/users/:username/movie/:id/favourites", () => {
+    describe("for valid user name", () => {
+      describe("when the movie is in favourites", () => {
+        it("should return user message and a status 201", () => {
+          return request(api)
+            .post(`/api/users/${users[0].username}/movie/114514/favourites`)
+            .set("Accept", "application/json")
+            .expect("Content-Type", "application/json; charset=utf-8")
+            .expect(201)
             .then((res) => {
-              expect(res.body.success).to.be.true;
-              expect(res.body.token).to.not.be.undefined;
-              user1token = res.body.token.substring(7);
+              expect(res.body).to.have.property("username", users[0].username);
+              expect(res.body.favourites.length).to.equal(0);
             });
         });
+      });
+      describe("when the movie is not in favourites", () => {
+        it("return error message and a status 404", () => {
+          return request(api)
+            .post(`/api/users/${users[0].username}/movie/77777/favourites`)
+            .set("Accept", "application/json")
+            .expect("Content-Type", /json/)
+            .expect(404)
+            .expect({ code: 404, msg: 'Not in favourites.' })
+        });
+      });
+    });
+    describe("for invalid user name", () => {
+      it("return error message and a status 404", () => {
+        return request(api)
+          .get(`/api/users/sadassdawqe/movie/114514/favourites`)
+          .set("Accept", "application/json")
+          .expect("Content-Type", "text/html; charset=utf-8")
+          .expect(404)
       });
     });
   });
